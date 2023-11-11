09fd0e8743d4cf28604ebb8f43592c971ea1eb48
coco-server <cocobird231@gmail.com>
2023-11-11 13:52:15 +0800

    Update launch files to ver 2.0 with service.json support.

--- launch/launch.py
+++ launch/launch.py
@@ -1,19 +1,27 @@
-#ver=1.4
+#ver=2.0
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 import os
+import json
 import yaml
 from yaml import load, Loader
 
+pkgName = 'cpp_timesyncserver'
+
 def generate_launch_description():
-    commonFilePath = os.path.join(get_package_share_directory('cpp_timesyncserver'), 'launch/common.yaml')
+    commonFilePath = os.path.join(get_package_share_directory(pkgName), 'launch/common.yaml')
     with open(commonFilePath, 'r') as f:
         data = yaml.load(f, Loader=Loader)
+
+    serviceFilePath = os.path.join(get_package_share_directory(pkgName), 'launch/service.json')
+    with open(serviceFilePath, 'r') as f:
+        serviceData = json.load(f)
+
     return LaunchDescription([
         Node(
-            package="cpp_timesyncserver",
+            package=pkgName,
             namespace=data['generic_prop']['namespace'],
             executable=data['launch_node'],
             output="screen",
@@ -24,16 +32,16 @@ def generate_launch_description():
                     # Do not change the settings rashly
                     "nodeName" : data['generic_prop']['nodeName'] + '_' + str(data['generic_prop']['id']) + '_node', 
                     "id" : data['generic_prop']['id'], 
-                    "devInfoService" : data['generic_prop']['devInfoService'], 
-                    "devInterface" : data['generic_prop']['devInterface'], 
-                    "devMultiNode" : data['generic_prop']['devMultiNode'], 
-                    "qosService" : data['generic_prop']['qosService'], 
-                    "qosDirPath" : data['generic_prop']['qosDirPath'], 
-                    "safetyService" : data['generic_prop']['safetyService'], 
-                    "timesyncService" : data['generic_prop']['timesyncService'], 
-                    "timesyncPeriod_ms" : data['generic_prop']['timesyncPeriod_ms'], 
-                    "timesyncAccuracy_ms" : data['generic_prop']['timesyncAccuracy_ms'], 
-                    "timesyncWaitService" : data['generic_prop']['timesyncWaitService'], 
+                    "devInfoService" : serviceData['devInfoService'], 
+                    "devInterface" : serviceData['devInterface'], 
+                    "devMultiNode" : serviceData['devMultiNode'], 
+                    "qosService" : serviceData['qosService'], 
+                    "qosDirPath" : serviceData['qosDirPath'], 
+                    "safetyService" : serviceData['safetyService'], 
+                    "timesyncService" : serviceData['timesyncService'], 
+                    "timesyncPeriod_ms" : serviceData['timesyncPeriod_ms'], 
+                    "timesyncAccuracy_ms" : serviceData['timesyncAccuracy_ms'], 
+                    "timesyncWaitService" : serviceData['timesyncWaitService'], 
                 }
             ]
         )
